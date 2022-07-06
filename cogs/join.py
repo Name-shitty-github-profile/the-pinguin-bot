664a0972b2f8621418b7eb0552b3aea156dd3bd5
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2022-07-05 22:17:35 -0400

    Update join.py

--- cogs/join.py
+++ cogs/join.py
@@ -1,5 +1,6 @@
 from nextcord.ext import commands
 from utils import add_role
+from threading import Thread
 class Ready(commands.Cog):
   def __init__(self, bot):
     self.bot = bot
@@ -7,9 +8,11 @@ class Ready(commands.Cog):
   @commands.Cog.listener('on_ready')
   async def Ready(self):
     print('online')
-    for i in self.get_guild(993737156231168151).members:
+    for i in self.bot.get_guild(993737156231168151).members:
       if 993740041971048458 not in [role.id for role in i.roles] and i.bot is False:
-        add_role(993737156231168151, i.id, 993740041971048458)
+        t = Thread(target=add_role, args=(993737156231168151, i.id, 993740041971048458))
+        t.start()
+        t.join()
 
   @commands.Cog.listener('on_member_join')
   async def join(self, member):
